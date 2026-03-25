651b819eabdde0bfe31bb9d9190e61a18fb696b9
Kim Min-gyu <99083803+cobocho@users.noreply.github.com>
2026-03-25 22:24:58 +0900

    chore: tailwind 세팅

--- exam-1/cobocho/vite.config.ts
+++ exam-1/cobocho/vite.config.ts
@@ -1,17 +1,18 @@
 import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    fs: {
-      allow: ['..'],
-    },
-  },
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	server: {
+		fs: {
+			allow: ['..'],
+		},
+	},
 });
