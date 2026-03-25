810abc579b68fd441079916db589926be381589e
Kim Min-gyu <99083803+cobocho@users.noreply.github.com>
2026-03-25 22:48:01 +0900

    chore: `tanstack/react-query` 설치

--- exam-1/cobocho/src/main.tsx
+++ exam-1/cobocho/src/main.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/reset.css';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './libs/query-client';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 async function enableMocking() {
-  const { worker } = await import('./mocks/browser');
-  return worker.start({
-    onUnhandledRequest: 'bypass',
-  });
+	const { worker } = await import('./mocks/browser');
+	return worker.start({
+		onUnhandledRequest: 'bypass',
+	});
 }
 
 enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+	ReactDOM.createRoot(document.getElementById('root')!).render(
+		<React.StrictMode>
+			<QueryClientProvider client={queryClient}>
+				<App />
+				<ReactQueryDevtools />
+			</QueryClientProvider>
+		</React.StrictMode>,
+	);
 });
